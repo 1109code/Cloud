9cfccff29f0e319334281dc7a9887eba71ca1668
YoungChanRho <0chan7@hanmail.net>
2022-11-15 11:14:41 +0900

    final_pjt

--- movies/models.py
+++ movies/models.py
@@ -2,14 +2,22 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=30)
+
+
 class Movie(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     overview = models.TextField()
-    genre = models.CharField(max_length=30)
     release_date = models.DateTimeField()
     poster_path = models.TextField()
-    actors = models.CharField(max_length=30)
+    genre = models.ManyToManyField(Genre, related_name='movies')
+    actors = models.ManyToManyField(Actor, related_name='movies')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -17,6 +25,6 @@ class Movie(models.Model):
         return self.title
 
 
-class Actor(models.Model):
 
-class 
+
+
